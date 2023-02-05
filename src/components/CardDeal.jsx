20b7f1b0b18763d1c2cfd6f8800784f7f5ebcf65
Victor Baziuk <vbaziuk13@gmail.com>
2023-02-05 16:38:36 -0500

    add clients section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/> in a few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel leo molestie, pretium sapien et, viverra velit. Proin porttitor nisl arcu, id tincidunt justo vulputate vel.</p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+    </div>
+
+
+  </section>
+)
 
 export default CardDeal
